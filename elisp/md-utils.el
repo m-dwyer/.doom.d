c2cb9722d94923b8a2c2ffffc947b214ba19d139
m-dwyer <13535330+m-dwyer@users.noreply.github.com>
2021-10-09 16:36:18 +1100

    Fix title and slug for org roam file

--- elisp/md-utils.el
+++ elisp/md-utils.el
@@ -27,23 +27,22 @@
   (let ((books-dir (expand-file-name "books" md--org-resources-dir)))
     (make-directory books-dir :parents))
 
-  (setq note-path
-        (expand-file-name "books/new.org" md--org-resources-dir))
-
   (setq book-metadata (get-book-metadata))
   (setq book-title (cdr (assoc 'title book-metadata)))
   (setq book-author (cdr (assoc 'author book-metadata)))
 
-  (org-roam-capture- :node (org-roam-node-create)
+  (org-roam-capture- :node (org-roam-node-create :title (progn book-title))
                      :templates `(("b" "book note" plain (function md/annotations-to-org-file)
                                   :target
                                    (file+head
-                                    ,note-path
                                     ,(concat
-                                     "#+title: %(plist-get org-capture-plist :title)\n"
+                                      "books/"
+                                      (format-time-string "%Y%m%d%H%M%S")
+                                      "-${slug}.org")
+                                    ,(concat
+                                     "#+title: ${title}\n"
                                      "#+author: %(plist-get org-capture-plist :author)\n"))
                                    :immediate-finish t
                                    :jump-to-captured t
                                    :empty-lines 1
-                                   :title ,book-title
                                    :author ,book-author))))
